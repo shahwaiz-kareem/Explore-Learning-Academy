b0341ea96095335cfc2079914ebc1e0dff7f43f6
shahwaiz-kareem <shah.dev23k@gmail.com>
2024-09-24 12:34:44 +0500

    bug fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: "build",
+
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/app/(admin)/dashboard/settings/page.jsx
+++ src/app/(admin)/dashboard/settings/page.jsx
@@ -3,18 +3,17 @@ import SettingsForm from "@/components/forms/settingsForm";
 
 const Page = async () => {
   const res = await getSettings();
-  const { feePerMonth, maxStudents, duration, shiftTiming1, shiftTiming2 } =
-    res;
+
   return (
     <div className="flex flex-col    p-4">
       <h1 className="text-3xl font-bold mb-4">Settings</h1>
       <div className="px-2 lg:px-8 ">
         <SettingsForm
-          updateFeePerMonth={feePerMonth}
-          updateMaxStudents={maxStudents}
-          updateDuration={duration}
-          updateShiftTiming1={shiftTiming1}
-          updateShiftTiming2={shiftTiming2}
+          updateFeePerMonth={res?.feePerMonth}
+          updateMaxStudents={res?.maxStudents}
+          updateDuration={res?.duration}
+          updateShiftTiming1={res?.shiftTiming1}
+          updateShiftTiming2={res?.shiftTiming2}
         />
       </div>
     </div>
